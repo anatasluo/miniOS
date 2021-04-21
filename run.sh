121c34753bc819369de83b378889b214e2e26ef6
anatasluo <luolongjuna@gmail.com>
2021-04-22 02:24:00 +0800

    update commdline

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ else
 	qemu-system-aarch64 -smp 4 -M virt -m 1024 -cpu cortex-a53 \
 		-kernel /home/anatasluo/Git/linux/arch/arm64/boot/Image \
 		-initrd ./rootfs/rootfs.cpio.gz \
-		-append 'nokaslr root=/dev/vda console=ttyAMA0' \
+		-append 'nokaslr console=ttyAMA0' \
 		-netdev user,id=mynet \
 		-device virtio-net-pci,netdev=mynet -nographic 
 fi
